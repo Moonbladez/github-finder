baf702b79fc0f74cb4f7a872c1d17e76b974d215
Bekki Young <moonbladez@hotmail.com>
2020-06-14 21:19:33 +0200

    added api data to single user component

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
+import { RiExternalLinkLine } from "react-icons/ri";
+import { Spinner } from "../../images/spinner.gif";
 
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
 	}
+
+	static propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		getUser: PropTypes.func.isRequired,
+	};
 	render() {
 		const {
 			name,
@@ -18,16 +29,76 @@ export class User extends Component {
 			public_repos,
 			public_gists,
 			hireable,
+			company,
 		} = this.props.user;
 
 		const { loading } = this.props;
 
 		return (
 			<div>
-				<h1>Single User: {location}</h1>
+				<Link to='/' className='btn btn-light'>
+					back to search
+				</Link>
+				Hireable:{" "}
+				{hireable ? (
+					<AiOutlineCheckCircle className='text-success' />
+				) : (
+					<AiOutlineCloseCircle className='text-danger' />
+				)}
+				<div className='card grid-2'>
+					<div className='all-center'>
+						<img src={avatar_url} className='round-img' alt={name} />
+						<h2>{name}</h2>
+						<p>Location: {location}</p>
+					</div>
+					<div>
+						{bio && (
+							<>
+								<h3>Bio</h3>
+								{bio}
+							</>
+						)}
+						<a href={html_url} className='btn btn-dark my-1'>
+							visit github profile
+						</a>
+						<ul>
+							<li>
+								{login && (
+									<>
+										<strong>username:</strong> {login}
+									</>
+								)}
+							</li>
+							<li>
+								{company && (
+									<>
+										<strong>company:</strong> {company}
+									</>
+								)}
+							</li>
+							<li>
+								{blog && (
+									<>
+										<strong>website:</strong>{" "}
+										<a href={`${blog}`} target='blank'>
+											{blog}
+											<RiExternalLinkLine />
+										</a>
+									</>
+								)}
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className='card text-center'>
+					<div className='badge badge-primary'>followers: {followers}</div>
+					<div className='badge badge-primary'>following: {following}</div>
+					<div className='badge badge-primary'>
+						public repos: {public_repos}
+					</div>
+					<div className='badge badge-primary'>gists: {public_gists}</div>
+				</div>
 			</div>
 		);
 	}
 }
-
-export default User;
